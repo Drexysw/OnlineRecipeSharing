56bcf5417605f8f2ce512c5d0971ba3c4c25a97a
Drexysw <knevdani@gmail.com>
2024-04-22 11:45:15 +0300

    Add project files.

---
+++ OnlineRecipeSharing/Program.cs
@@ -0,0 +1,36 @@
+using OnlineRecipeSharing.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddApplicationDbContext(builder.Configuration);
+builder.Services.AddApplicationIdentity(builder.Configuration);
+
+builder.Services.AddApplicationServiceCollection();
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();    
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+await app.RunAsync();
